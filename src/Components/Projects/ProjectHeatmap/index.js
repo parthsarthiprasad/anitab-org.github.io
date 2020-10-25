122cc9be7d14afde6286ea3a28c8c1845acf1957
parth sarthi prasad <mepsp2000@gmail.com>
2020-10-25 20:24:08 +0530

    development in backend fetch, issues with asnchronous chaining

--- src/Components/Projects/ProjectHeatmap/index.js
+++ src/Components/Projects/ProjectHeatmap/index.js
@@ -1,11 +1,14 @@
 import React,{useEffect, useState} from 'react'
 import {ActivityIndicator,View,Text, StyleSheet} from 'react-native'
 
-function DataIcon({all}){
-    console.log(all.length)
+function DataIcon(props){
+    console.log(props)
     return(
         <View>
-            {all}
+            <Text>
+            {/* {props} */}
+            hi
+            </Text>
         </View>
     )
 }
@@ -17,16 +20,20 @@ function ProjectHeatmap() {
         fetch("https://api.github.com/orgs/anitab-org/repos")
         .then(res => res.json())
         .then((res) =>{
-            Promise.all(
-                res.map(
-                    
-                    element =>fetch(element.url + "/stats/participation")
-                             .then(data =>data.json())
-                )
-            ).then((datas)=>{setdata(datas)})
-            .catch((error)=> console.log(error))
-            .finally(()=> setisLoading(false))
+            var tdata = []
+            res.forEach((element) => {
+                var uri = element.url + "/stats/commit_activity";
+                fetch({uri})
+                .then(data => data.json())
+                .then(data => tdata.push(data))
+                .catch(error => console.log(error))
+            })
+            return tdata;
         })
+        .then(data => setdata(data))
+        .catch((error)=> console.log(error, "unable to fetch repo data"))
+        .finally(()=> setisLoading(false))
+        
         
     }, []);
     
